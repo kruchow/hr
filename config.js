f568a7b4495aaa9a882f571138bbf4a7b3b0d9e9
chawalit dangpeng <107448001+kruchow@users.noreply.github.com>
2026-09-19 10:52:55 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// config.js - ตัวแปรกำหนดค่าเชื่อมต่อ Supabase
+const SUPABASE_URL = "https://oyrqokngimngsdpjarxg.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_KyQOOgc36z2tZKaX9OKa7w_TVJEEnYV";
